b7fba2587885239d836201d4a981c924164f1b22
mamartinezGH <mmariano358@gmail.com>
2025-11-26 08:38:34 -0300

    Mejoras en home

---
+++ frontend/src/app/pages/preguntas-frecuentes/preguntas-frecuentes.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-preguntas-frecuentes',
+  imports: [],
+  templateUrl: './preguntas-frecuentes.html',
+  styleUrl: './preguntas-frecuentes.css'
+})
+export class PreguntasFrecuentesComponent {
+
+}
